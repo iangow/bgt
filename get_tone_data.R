e19ff0edacc9de8eda3ff67b7402f87699e82fb2
Ian Gow <igow@hbs.edu>
2014-07-13 13:57:15 -0400

    Added code to pull word counts, as well as tone data.

--- get_tone_data.R
+++ get_tone_data.R
@@ -16,7 +16,8 @@ addToneData <- function(file_name) {
             FROM streetevents.speaker_data
             WHERE speaker_name != 'Operator' AND file_name ='", file_name, "')
         SELECT file_name, category, 
-            tone_count(string_agg(speaker_text, ' ')) AS tone_count
+            tone_count(string_agg(speaker_text, ' ')) AS tone_count,
+            word_count(string_agg(speaker_text, ' ')) AS word_count
         FROM raw_data
         GROUP BY file_name, category"))
     
@@ -35,7 +36,7 @@ addToneData <- function(file_name) {
     tone_data <- cbind(subset(tone_raw, TRUE, select=-tone_count), tone_data)
     
     # Put data back in database.
-    dbWriteTable(pg, c("bgt", "tone"), tone_data,
+    dbWriteTable(pg, c("bgt", "word_counts"), tone_data,
                  append=TRUE, row.names=FALSE)
     
     rs <- dbDisconnect(pg)
@@ -45,16 +46,19 @@ addToneData <- function(file_name) {
 library(RPostgreSQL)
 pg <- dbConnect(PostgreSQL())
 
+dbGetQuery(pg, "DROP TABLE IF EXISTS bgt.word_counts")
+
 # Get a list of file names for which we need to get tone data.
 file_names <-  dbGetQuery(pg, "
-  SELECT DISTINCT file_name
-  FROM streetevents.calls
-  WHERE call_type=1 AND file_name NOT IN (SELECT file_name FROM bgt.tone)")
+    SELECT DISTINCT file_name
+    FROM streetevents.calls
+    WHERE call_type=1 -- AND file_name NOT IN (SELECT file_name FROM bgt.word_counts)")
 
 # Apply function to get tone data. Run on 12 cores.
 library(parallel)
 system.time(temp <- mclapply(file_names$file_name, addToneData, mc.cores=8))
 rs <- dbGetQuery(pg, "
     SET maintenance_work_mem='1GB';
-    CREATE INDEX ON bgt.tone (file_name)")
+    CREATE INDEX ON bgt.word_counts (file_name)")
 rs <- dbDisconnect(pg)
+
