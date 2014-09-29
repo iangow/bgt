7ebf537765db5b81e43cd4ea1c57f926ebfa2f00
Ian Gow <igow@hbs.edu>
2014-09-29 14:25:56 -0400

    Tweaked code for long words to require more than three letters for any three-syllable word.

--- word_freq.sql
+++ word_freq.sql
@@ -1,4 +1,9 @@
-﻿WITH link_table AS (
+DROP TABLE IF EXISTS bgt.word_freq;
+
+SET work_mem='12GB';
+
+CREATE TABLE bgt.word_freq AS 
+WITH link_table AS (
     SELECT a.file_name, a.permno, b.call_date::date, siccd,
         floor(c.siccd/100)::int AS sic2
     FROM streetevents.crsp_link AS a
@@ -6,14 +11,10 @@
     USING (file_name)
     INNER JOIN crsp.stocknames AS c
     ON a.permno=c.permno AND b.call_date >= c.namedt 
-        AND (b.call_date <= c.nameenddt OR c.nameenddt IS NULL)
-    LIMIT 100),
-long_words AS (
-    SELECT sic2, file_name, getLongWords(string_agg(speaker_text, ' ')) AS long_words
-    FROM streetevents.speaker_data
-    INNER JOIN link_table
-    USING (file_name)  
-    GROUP BY sic2, file_name)
-SELECT * -- sic2, getFreqDist(array_agg(unnest(long_words))) AS words
-FROM long_words
--- GROUP BY sic2   
+        AND (b.call_date <= c.nameenddt OR c.nameenddt IS NULL))
+   
+SELECT sic2, count_agg(long_words) AS long_words
+FROM bgt.long_words
+INNER JOIN link_table
+USING (file_name)  
+GROUP BY sic2;
