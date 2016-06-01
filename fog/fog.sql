25f183356d8645ca875de02b6a3372cc1da64c12
Ian Gow <iandgow@gmail.com>
2016-06-01 15:17:04 -0400

    Tweaks to fog SQL function.

--- fog/fog.sql
+++ fog/fog.sql
@@ -1,36 +1,51 @@
 ﻿-- CREATE EXTENSION plperlu;
--- CREATE TYPE fog_stats AS (fog float8, num_words integer, percent_complex float8, num_sentences integer);
+-- CREATE EXTENSION plperlu;
+DROP TYPE fog_stats CASCADE;
+
+CREATE TYPE fog_stats AS (fog float8, num_words integer, percent_complex float8, 
+                                num_sentences integer, fog_original float8, num_sentences_original integer);
 
 CREATE OR REPLACE FUNCTION fog_data(text) RETURNS fog_stats AS $$ 
 
-  # Load Perl module that calculates fog, etc.
-  use Lingua::EN::Fathom;
-  use Lingua::EN::Sentence qw( get_sentences add_acronyms );
+    # Load Perl module that calculates fog, etc.
+    use Lingua::EN::Fathom;
+    use Lingua::EN::Sentence qw( get_sentences add_acronyms );
 
-  my $text = new Lingua::EN::Fathom;
-  if (defined($_[0])) {
-    $text->analyse_block($_[0]);
-    $fog   = $text->fog;
-    $num_words = $text->num_words;
-    $percent_complex = $text->percent_complex_words;  
-    $num_sentences = $text->num_sentences;
-  }
+    if (defined($_[0])) {
+        my $text = new Lingua::EN::Fathom;
+
+        my $sentences=get_sentences($_[0]);   ## Get the sentences.
+        $num_sentences =@$sentences;
+    
+        $text->analyse_block($_[0]);
+        $num_sentences_original = $text->num_sentences;
+        $num_words = $text->num_words;
+        $percent_complex = $text->percent_complex_words;  
+        $fog = 0.4 * ($num_words/$num_sentences + $percent_complex);
+        $fog_original = $text->fog;
+    }
 
   return {fog => $fog, num_words => $num_words, percent_complex => $percent_complex, 
-	num_sentences => $num_sentences};
+	num_sentences => $num_sentences, fog_original => $fog_original, num_sentences_original => $num_sentences_original};
 
 $$ LANGUAGE plperlu;
 
 CREATE OR REPLACE FUNCTION fog(text) RETURNS float8 AS $$ 
 
-  # Load Perl modules that calculate fog, etc.
-  use Lingua::EN::Fathom;
-  use Lingua::EN::Sentence qw( get_sentences add_acronyms );
+    use Lingua::EN::Fathom;
+    use Lingua::EN::Sentence qw( get_sentences add_acronyms );
 
-  my $text = new Lingua::EN::Fathom;
-  if (defined($_[0])) {
-    $text->analyse_block($_[0]);
-    return($text->fog);
-  }
+    if (defined($_[0])) {
+        my $text = new Lingua::EN::Fathom;
 
+        my $sentences=get_sentences($_[0]);   ## Get the sentences.
+        $num_sentences =@$sentences;
+	
+        $text->analyse_block($_[0]);
+        $num_words = $text->num_words;
+        $percent_complex = $text->percent_complex_words;  
+        $fog = 0.4 * ($num_words/$num_sentences + $percent_complex);
+        return($fog);
+    }
+    
 $$ LANGUAGE plperlu;
