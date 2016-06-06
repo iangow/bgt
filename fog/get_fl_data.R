a744cc384377cb12057e240bc69a315b0308b6d8
Ian Gow <iandgow@gmail.com>
2016-06-06 11:08:40 -0400

    Minor clean-up to code for fog and FL data.
    - Fog data aggregated by "category"
    - FL data are on forward-looking sentences.

--- fog/get_fl_data.R
+++ fog/get_fl_data.R
@@ -4,7 +4,7 @@ pg <- dbConnect(PostgreSQL())
 
 if (!dbExistsTable(pg, c("bgt", "fl_data"))) {
     rs <- dbGetQuery(pg, "
-        CREATE TABLE bgt.fl_data 
+        CREATE TABLE bgt.fl_data
             (file_name text, category text, prop_fl_sents float8)")
 
     rs <- dbGetQuery(pg, "CREATE INDEX ON bgt.fl_data (file_name)")
@@ -15,17 +15,17 @@ rs <- dbDisconnect(pg)
 # Make a function to count forward-looking words ----
 add_fl_data <- function(file_name) {
     # Function to get word count data for all utterances in a call
-    
+
     library(RPostgreSQL)
     pg <- dbConnect(PostgreSQL())
-    
+
     # Get tone data. Data is JSON converted to text.
     rs <- dbGetQuery(pg, paste0("
-        
+
         WITH raw_data AS (
-            SELECT file_name, speaker_name, speaker_number, 
+            SELECT file_name, speaker_name, speaker_number,
                 unnest(sent_tokenize(speaker_text)) AS sents,
-                (CASE WHEN role='Analyst' THEN 'anal' ELSE 'comp' END) 
+                (CASE WHEN role='Analyst' THEN 'anal' ELSE 'comp' END)
                     || '_' || context AS category
             FROM streetevents.speaker_data
             WHERE speaker_name != 'Operator' AND file_name = '", file_name, "')
@@ -33,9 +33,9 @@ add_fl_data <- function(file_name) {
         SELECT file_name, category, prop_fl_sents(array_agg(sents))
         FROM raw_data
         GROUP BY file_name, category"))
-     
+
     rs <- dbDisconnect(pg)
-    
+
 }
 
 # Get list of files to process ----
@@ -53,4 +53,3 @@ file_names <-  dbGetQuery(pg, "
 library(parallel)
 system.time(temp <- mclapply(file_names$file_name, add_fl_data, mc.cores=8))
 rs <- dbDisconnect(pg)
-
--- fog/get_fog_data.R
+++ fog/get_fog_data.R
@@ -2,16 +2,15 @@
 # Create a table to store the data ----
 library(RPostgreSQL)
 pg <- dbConnect(PostgreSQL())
-    
+
 if (!dbExistsTable(pg, c("bgt", "fog"))) {
     rs <- dbGetQuery(pg, "
-        CREATE TABLE bgt.fog 
-            (file_name text, category text, 
-                fog float8, 
-                num_words integer,
-                percent_complex float8,
-                num_sentences integer)")
-    
+        CREATE TABLE bgt.fog
+            (file_name text, category text,
+             fog float8, num_words integer, percent_complex float8,
+             num_sentences integer, fog_original float8,
+             num_sentences_original integer)")
+
     rs <- dbGetQuery(pg, "CREATE INDEX ON bgt.fog (file_name, category)")
 }
 rs <- dbDisconnect(pg)
@@ -20,10 +19,10 @@ rs <- dbDisconnect(pg)
 get_fog_data <- function(file_name) {
     # Function to get statistics for within-call regressions
     # of fog of answers on fog of questions.
-    
+
     library(RPostgreSQL)
     pg <- dbConnect(PostgreSQL())
-    
+
     # Get fog data
     reg_data <- dbGetQuery(pg, paste0("
         INSERT INTO bgt.fog
@@ -33,33 +32,39 @@ get_fog_data <- function(file_name) {
                     AS category, speaker_text
             FROM streetevents.speaker_data
             WHERE file_name='", file_name, "' AND speaker_name != 'Operator'),
-        
+
         call_text AS (
             SELECT file_name, category, string_agg(speaker_text, ' ') AS all_text
             FROM raw_data
             GROUP BY file_name, category)
-            
+
         SELECT file_name, category, (fog_data(all_text)).*
         FROM call_text"))
-    
+
     dbDisconnect(pg)
 }
 
 # Get list of files without fog data ----
+library(dplyr)
+pg <- src_postgres()
 
-pg <- dbConnect(PostgreSQL())
+calls <- tbl(pg, sql("SELECT *  FROM streetevents.calls"))
 
-# Get a list of file names for which we need to get tone data.
-file_names <-  dbGetQuery(pg, "
-    SELECT DISTINCT file_name
-    FROM streetevents.calls
-    WHERE call_type=1 AND file_name NOT IN (SELECT file_name FROM bgt.fog)")
-rs <- dbDisconnect(pg)
+processed <- tbl(pg, sql("SELECT * FROM bgt.fog"))
+
+file_names <-
+    calls %>%
+    filter(call_type==1L) %>%
+    anti_join(processed) %>%
+    select(file_name) %>%
+    distinct() %>%
+    as.data.frame(n=-1)
 
 # Apply function to get fog data ----
 # Run on 12 cores.
 pg <- dbConnect(PostgreSQL())
 library(parallel)
-system.time(temp <- mclapply(file_names$file_name[1:100], get_fog_data, mc.cores=12))
+system.time(temp <- mclapply(file_names$file_name,
+                             get_fog_data, mc.cores=8))
 
 rs <- dbDisconnect(pg)
