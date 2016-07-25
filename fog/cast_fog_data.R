c543158f7cbb29342e05d7d2b5965078e12ae617
Ian Gow <iandgow@gmail.com>
2016-07-25 17:31:36 -0400

    Edits to code for casting fog data.
    - Use dplyr.
    - Include fog decomposition.

--- fog/cast_fog_data.R
+++ fog/cast_fog_data.R
@@ -1,45 +1,106 @@
 # Get fog data ----
-library(RPostgreSQL)
-pg <- dbConnect(PostgreSQL())
-library(reshape2)
+library(dplyr)
+library(tidyr)
+pg <- src_postgres()
 
-fog.data <- dbGetQuery(pg, "
-    -- Code to aggregate speaker-level fog data
-    -- by 'category' (analyst/company, presentation/Q&A)
-    WITH raw_data AS (
-        SELECT file_name, category,
-            num_words * percent_complex/100.0 AS num_complex_words,
-            num_sentences, num_words, percent_complex,
-            prop_fl_sents AS prop_fl_sentences
-        FROM bgt.fog
-        INNER JOIN bgt.fl_data
-        USING (file_name, category))
-    SELECT *,
-        (num_words::float8/num_sentences + 100*
-            num_complex_words/num_words) * 0.4 AS fog
-    FROM raw_data")
-
-# Recast data so that three rows expand into 3*K columns
-cast.data <- NULL
-for (i in setdiff(names(fog.data), c("file_name", "category"))) {
-    temp <- cast(fog.data, file_name ~ category, value=i)
-    new.names <- paste(i, names(temp)[2:length(names(temp))], sep="_")
-                             names(temp) <- c("file_name", new.names)
-    if (is.null(cast.data)) {
-        cast.data <- temp
-    } else {
-        cast.data <- merge(cast.data, temp, all=TRUE)
-    }
+tbl_pg <- function(table) {
+    tbl(pg, sql(paste0("SELECT * FROM bgt.", table)))
 }
 
-rs <- dbWriteTable(pg, c("bgt", "fog_recast"), cast.data,
-                         overwrite=TRUE, row.names=FALSE)
+fl_data <- tbl_pg("fl_data")
+sent_counts <- tbl_pg("sent_counts")
+tone_data <- tbl_pg("tone_data")
+long_words <- tbl_pg("long_words")
+ticker_match <- tbl_pg("ticker_match")
 
-rs <- dbGetQuery(pg, "
+calls <-
+    tbl(pg, sql("SELECT * FROM streetevents.calls"))
+
+latest_calls <-
+    calls %>%
+    group_by(file_name) %>%
+    summarize(last_update=max(last_update)) %>%
+    inner_join(calls) %>%
+    select(file_name, last_update, call_date)
+
+
+fog <-
+    tbl_pg("fog") %>%
+    mutate(num_complex_words = percent_complex / 100 * num_words)
+
+fog_jargon_sql <-
+    sql("CASE WHEN num_complex_words > 0
+        THEN 0.4*percent_complex*num_jargon_words/num_complex_words END")
+fog_special_sql <-
+    sql("CASE WHEN num_complex_words > 0
+        THEN 0.4*percent_complex*(1-num_jargon_words/num_complex_words) END")
+fog_words_sent_correct_sql <-
+    sql("CASE WHEN num_sentences > 0
+        THEN 0.4*num_words/num_sentences END")
+fog_words_sent_error_sql <-
+    sql("CASE WHEN num_sentences > 0 AND num_sentences_original > 0
+        THEN 0.4*(num_words/num_sentences-num_words/num_sentences_original) END")
+
+fog_decomposed <-
+    tbl_pg("jargon_words") %>%
+    group_by(file_name, category) %>%
+    summarize(num_jargon_words=sql("max(num_jargon_words)::float8")) %>%
+    inner_join(fog %>% select(-num_sentences)) %>%
+    inner_join(sent_counts) %>%
+    mutate(num_words=sql("num_words::float8")) %>%
+    mutate(fog_jargon=fog_jargon_sql,
+           fog_special=fog_special_sql,
+           fog_words_sent_correct=fog_words_sent_correct_sql,
+           fog_words_sent_error=fog_words_sent_error_sql) %>%
+    compute()
+
+within_call_data <- tbl_pg("within_call_data")
+
+call_level_tone_data <-
+    tone_data %>%
+    select(-category) %>%
+    group_by(file_name) %>%
+    summarize_each(funs(sum))
+
+fog.data <-
+    fl_data %>%
+    inner_join(sent_counts) %>%
+    rename(num_sentences_fl=num_sentences) %>%
+    inner_join(tone_data) %>%
+    inner_join(fog_decomposed) %>%
+    distinct() %>%
+    collect(n=Inf)
+
+# Recast data so that three rows expand into 3*K columns
+cast.data <-
+    fog.data %>%
+    gather(key, value, -file_name, -category) %>%
+    unite(var, c(key, category), sep="_") %>%
+    spread(var, value) %>%
+    tbl_df() %>%
+    select(-ends_with("anal_pres")) %>%
+    inner_join(call_level_tone_data %>%
+                   inner_join(within_call_data) %>%
+                   select(-last_update) %>%
+                   collect(n=Inf))
+
+fog.data %>%
+    mutate(same_sent_count = num_sentences_fl==num_sentences_original) %>%
+    group_by(same_sent_count) %>%
+    summarize(count=n())
+
+# Send data to database ----
+rs <-
+    RPostgreSQL::dbWriteTable(pg$con, c("bgt", "fog_recast"), cast.data,
+                   overwrite=TRUE, row.names=FALSE)
+
+rs <-
+    RPostgreSQL::dbGetQuery(pg$con, "
         SET maintenance_work_mem='5GB';
         CREATE INDEX ON bgt.fog_recast (file_name)")
 
-merged.fog.data <- dbGetQuery(pg, "
+merged.fog.data <-
+    tbl(pg, sql("
     SELECT DISTINCT *,
         0.4 * (100*(num_complex_words_comp_qa+num_complex_words_anal_qa)/
         (num_words_comp_qa+num_words_anal_qa) +
@@ -47,12 +108,22 @@ merged.fog.data <- dbGetQuery(pg, "
             (num_sentences_comp_qa+num_sentences_anal_qa)) AS fog_qa
     FROM bgt.ticker_match
     INNER JOIN bgt.fog_recast
-    USING (file_name)")
+    USING (file_name)"))
 
-library(foreign)
+merged.fog.data
+merged.fog.data %>% summarize(n())
 
-write.dta(merged.fog.data, file="~/Dropbox/research/BuGT/data/merged_fog_data.dta",
-          version=11L)
-head(merged.fog.data)
-dim(merged.fog.data)
-table(merged.fog.data$datadate)
+merged.fog.data %>%
+    mutate(year=sql("extract(year from datadate)")) %>%
+    group_by(year) %>%
+    summarize(n()) %>%
+    arrange(year) %>%
+    print(n=100)
+
+long_words %>%
+    inner_join(latest_calls) %>%
+    mutate(year=sql("extract(year from call_date)")) %>%
+    group_by(year) %>%
+    summarize(n()) %>%
+    arrange(year) %>%
+    print(n=100)
