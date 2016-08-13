cdb3e218e1385937d231159a439725350d67d4e2
Ian Gow <iandgow@gmail.com>
2016-08-13 04:21:28 -0400

    Added last_update to sent_counts table.

--- fog/get_sent_count.R
+++ fog/get_sent_count.R
@@ -5,8 +5,8 @@ pg <- dbConnect(PostgreSQL())
 if (!dbExistsTable(pg, c("bgt", "sent_counts"))) {
     rs <- dbGetQuery(pg, "
         CREATE TABLE bgt.sent_counts
-            (file_name text, category text,
-                num_sentences integer)")
+            (file_name text, last_update timestamp without time zone,
+             category text, num_sentences integer)")
 
     rs <- dbGetQuery(pg, "CREATE INDEX ON bgt.sent_counts (file_name)")
 }
@@ -24,20 +24,21 @@ get_sent_count <- function(file_name) {
     reg_data <- dbGetQuery(pg, paste0("
         SET work_mem='1GB';
 
-        INSERT INTO bgt.sent_counts
+        INSERT INTO bgt.sent_counts (file_name, last_update, category, num_sentences)
         WITH raw_data AS (
-            SELECT file_name,
+            SELECT file_name, last_update,
                 (CASE WHEN role='Analyst' THEN 'anal' ELSE 'comp' END) || '_' || context
                     AS category, speaker_text
             FROM streetevents.speaker_data
             WHERE file_name='", file_name, "' AND speaker_name != 'Operator'),
 
         call_text AS (
-            SELECT file_name, category, string_agg(speaker_text, ' ') AS all_text
+            SELECT file_name, last_update, category,
+                string_agg(speaker_text, ' ') AS all_text
             FROM raw_data
-            GROUP BY file_name, category)
+            GROUP BY file_name, last_update, category)
 
-        SELECT file_name, category, sent_count(all_text) AS num_sentences
+        SELECT file_name, last_update, category, sent_count(all_text) AS num_sentences
         FROM call_text"))
 
     dbDisconnect(pg)
@@ -57,7 +58,7 @@ file_names <-
     anti_join(processed) %>%
     select(file_name) %>%
     distinct() %>%
-    as.data.frame(n=-1)
+    as.data.frame(n=Inf)
 
 # Apply function to get sentence count data data ----
 # Run on 12 cores.
