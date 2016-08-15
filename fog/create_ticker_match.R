432f6bee60e89e61fc5d66b655b3d9ac43e53dc2
Ian Gow <iandgow@gmail.com>
2016-08-15 10:12:56 -0400

    Tweaks to ticker-match code.

--- fog/create_ticker_match.R
+++ fog/create_ticker_match.R
@@ -1,58 +1,64 @@
 #
-library(RPostgreSQL)
-pg <- dbConnect(PostgreSQL())
-# library(reshape)
 library(dplyr)
 
 pg <- src_postgres()
 
-linktable <-
-    tbl(pg, sql("SELECT * FROM crsp.ccmxpf_linktable")) %>%
-    filter(usedflag==1L, linkprim %in% c('C', 'P')) %>%
+# Some tickers have *s in them, so I clean them up
+calls <- tbl(pg, sql("SELECT * FROM streetevents.calls"))
+
+ccmxpf_lnkhist <-
+    tbl(pg, sql("SELECT * FROM crsp.ccmxpf_lnkhist"))
+
+crsp_linktable <-
+    ccmxpf_lnkhist %>%
+    filter(linktype %in% c('LC', 'LU', 'LS')) %>%
     mutate(permno=as.integer(lpermno)) %>%
-    select(gvkey, permno, linkdt, linkenddt)
+    select(gvkey, permno, linkdt, linkenddt) %>%
+    compute(indexes="gvkey")
 
-# - Some tickers have *s in them, so I clean them out.
-calls <-
-    tbl(pg, sql("SELECT * FROM streetevents.calls")) %>%
-    mutate(ticker=regexp_replace(ticker, '[*]', '', 'g')) %>%
+fundq <- tbl(pg, sql("SELECT * FROM comp.fundq"))
+secm <- tbl(pg, sql("SELECT * FROM comp.secm"))
+
+tickers <-
+    calls %>%
     mutate(call_date=sql("call_date::date")) %>%
-    select(file_name, co_name, call_desc, call_date, last_update, ticker) %>%
-    group_by(file_name) %>%
-    filter(last_update==max(last_update))
+    mutate(ticker=sql("regexp_replace(ticker, '[*]', '', 'g')")) %>%
+    select(file_name, last_update, ticker, call_date)
 
-fundq <-
-    tbl(pg, sql("SELECT * FROM comp.fundq")) %>%
-    mutate(month=eomonth(rdq)) %>%
-    select(gvkey, datadate, rdq, conm, month)
-
-secm <-
-    tbl(pg, sql("SELECT * FROM comp.secm")) %>%
-    mutate(month=eomonth(datadate)) %>%
-    select(gvkey, month, tic)
+secm_tickers <-
+    secm %>%
+    mutate(eomonth=eomonth(datadate)) %>%
+    select(gvkey, eomonth, datadate, tic) %>%
+    rename(ticker=tic) %>%
+    compute()
 
 rdqs <-
     fundq %>%
-    left_join(secm) %>%
-    select(gvkey, tic, rdq, conm, datadate) %>%
-    rename(ticker=tic) %>%
-    distinct() %>%
-    compute()
+    select(gvkey, rdq) %>%
+    mutate(eomonth=eomonth(rdq)) %>%
+    distinct()
+
+# Merge tickers from comp.secm with announcement dates
+# from comp.fundq. Add PERMNOs.
+comp_tickers <-
+    rdqs %>%
+    inner_join(secm_tickers) %>%
+    inner_join(crsp_linktable) %>%
+    filter(rdq >= linkdt,
+           rdq <= linkenddt | is.na(linkenddt)) %>%
+    select(ticker, rdq, gvkey, permno) %>%
+    compute
 
 # Match earnings announcements with calls within three days
 # and with the same ticker
 ticker_match <-
-    calls %>%
-    inner_join(rdqs) %>%
-    filter(between(call_date, rdq, sql("rdq + interval '3 days'")))
-
-ticker_match %>%
-    left_join(linktable) %>%
-    filter(datadate >= linkdt, datadate <= linkenddt || is.na(linkenddt)) %>%
+    tickers %>%
+    inner_join(comp_tickers) %>%
+    filter(between(call_date, rdq, sql("rdq + interval '3 days'"))) %>%
+    select(file_name, last_update, gvkey, permno) %>%
     compute(name="ticker_match", indexes="file_name", temporary=FALSE)
-rs <-
-    RPostgreSQL::dbGetQuery(pg$con, "DROP TABLE IF EXISTS bgt.ticker_match")
-rs <- RPostgreSQL::dbGetQuery(pg$con, "ALTER TABLE ticker_match SET SCHEMA bgt")
 
-# Merge tickers from comp.secm with announcement dates
-# from comp.fundqrdqs <-
+rs <-
+    RPostgreSQL::dbGetQuery(pg$con, "DROP TABLE IF EXISTS streetevents.ticker_match")
+rs <- RPostgreSQL::dbGetQuery(pg$con, "ALTER TABLE ticker_match SET SCHEMA streetevents")
+
