36299166ed2e44cb4f120643cf4731ec1cc33c4c
Ian Gow <iandgow@gmail.com>
2016-08-17 12:51:52 -0400

    Added number of sentences to forward-looking data.
    - Number of sentences is needed to aggregate by call.

--- fog/get_fl_data.R
+++ fog/get_fl_data.R
@@ -6,7 +6,7 @@ if (!dbExistsTable(pg, c("bgt", "fl_data"))) {
     rs <- dbGetQuery(pg, "
         CREATE TABLE bgt.fl_data
             (file_name text, last_update timestamp without time zone,
-                     category text, prop_fl_sents float8)")
+                     category text, prop_fl_sents float8, num_sentences integer)")
 
     rs <- dbGetQuery(pg, "CREATE INDEX ON bgt.fl_data (file_name)")
 }
@@ -30,8 +30,9 @@ add_fl_data <- function(file_name) {
                     || '_' || context AS category
             FROM streetevents.speaker_data
             WHERE speaker_name != 'Operator' AND file_name = '", file_name, "')
-        INSERT INTO bgt.fl_data (file_name, last_update, category, prop_fl_sents)
-        SELECT file_name, last_update, category, prop_fl_sents(array_agg(sents))
+        INSERT INTO bgt.fl_data (file_name, last_update, category, prop_fl_sents, num_sentences)
+        SELECT file_name, last_update, category, prop_fl_sents(array_agg(sents)),
+            array_length(array_agg(sents), 1) AS num_sentences
         FROM raw_data
         GROUP BY file_name, last_update, category"))
 
@@ -58,4 +59,4 @@ file_names <-
 # Apply function to get data on forward-looking words ----
 # Run on 12 cores.
 library(parallel)
-system.time(temp <- mclapply(file_names$file_name, add_fl_data, mc.cores=8))
+system.time(temp <- mclapply(file_names$file_name, add_fl_data, mc.cores=12))
