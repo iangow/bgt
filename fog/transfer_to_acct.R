c317c869d84b015d5338d5720e8277d937b9de12
Ian Gow <iandgow@gmail.com>
2016-09-05 16:47:02 -0400

    Filtered link to GVKEY based on RDQs.

--- fog/transfer_to_acct.R
+++ fog/transfer_to_acct.R
@@ -48,6 +48,7 @@ rdqs <-
 rdq_link <-
     rdqs %>%
     inner_join(crsp_linktable) %>%
+    filter(rdq >= linkdt, rdq <= linkenddt | is.na(linkenddt)) %>%
     select(gvkey, permno, rdq) %>%
     compute()
 
