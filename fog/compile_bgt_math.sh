55ba760f146e339f26b0d8988c8669ac8aad9f2b
Ian Gow <iandgow@gmail.com>
2017-09-04 17:07:30 +1000

    Deleted obsolete code.

--- fog/compile_bgt_math.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd fog
-ipython nbconvert --to latex --execute bgt_math.ipynb
-pdflatex >/dev/null bgt_math.tex
-rm bgt_math.out bgt_math.log bgt_math.aux
-open bgt_math.pdf
-# cd ..
