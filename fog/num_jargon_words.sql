67ee997d2da12f57906555553082a8c516aa6f0e
Ian Gow <iandgow@gmail.com>
2017-09-12 18:31:22 +1000

    Tweaked jargon words code to use SICs table.

--- fog/num_jargon_words.sql
+++ fog/num_jargon_words.sql
@@ -4,28 +4,12 @@ DROP TABLE IF EXISTS bgt.jargon_words;
 
 CREATE TABLE bgt.jargon_words AS
 WITH
-latest_calls AS (
-    SELECT file_name, max(last_update) AS last_update
-    FROM streetevents.calls
-    GROUP BY file_name),
 
-link_table AS (
-    SELECT a.file_name,
-        min(floor(c.siccd/100))::int AS sic2
-    FROM streetevents.crsp_link AS a
-    INNER JOIN streetevents.calls AS b
-    USING (file_name)
-    INNER JOIN crsp.stocknames AS c
-    ON a.permno=c.permno AND b.start_date >= c.namedt
-        AND (b.start_date <= c.nameenddt OR c.nameenddt IS NULL)
-    GROUP BY a.file_name)
 SELECT file_name, last_update, category,
     array_length(array_overlap(b.long_words,
     top_words(d.long_words, 100)), 1) AS num_jargon_words
 FROM bgt.long_words AS b
-INNER JOIN latest_calls
-USING (file_name, last_update)
-INNER JOIN link_table AS c
+INNER JOIN bgt.sics AS c
 USING (file_name)
 INNER JOIN bgt.word_freq AS d
 USING (sic2);
