d4557c5024f650e6012926d86d26c1cd35fe2bbf
Ian Gow <iandgow@gmail.com>
2017-09-12 18:30:30 +1000

    Moved word_freq code to R file.

---
+++ fog/create_word_freq.R
@@ -0,0 +1,29 @@
+library(RPostgreSQL)
+library(dplyr, warn.conflicts = FALSE)
+pg <- dbConnect(PostgreSQL())
+
+calls <- tbl(pg, sql("SELECT * FROM streetevents.calls"))
+sics <- tbl(pg, sql("SELECT * FROM bgt.sics"))
+long_words <- tbl(pg, sql("SELECT * FROM bgt.long_words"))
+
+dbGetQuery(pg, "SET work_mem='48GB'")
+
+latest_calls <-
+    calls %>%
+    group_by(file_name) %>%
+    summarize(last_update = max(last_update)) %>%
+    compute(index=c("file_name"))
+
+word_freq <-
+    latest_calls %>%
+    inner_join(sics) %>%
+    inner_join(long_words) %>%
+    group_by(sic2) %>%
+    summarize(long_words = count_agg(long_words)) %>%
+    compute(name = "word_freq", temporary = FALSE)
+
+dbGetQuery(pg, "DROP TABLE IF EXISTS bgt.word_freq")
+dbGetQuery(pg, "ALTER TABLE word_freq OWNER TO bgt")
+dbGetQuery(pg, "ALTER TABLE word_freq SET SCHEMA bgt")
+
+pg <- dbDisconnect(pg)
--- fog/get_jargon_words.sh
+++ fog/get_jargon_words.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-psql -f fog/word_freq.sql
 psql -f fog/num_jargon_words.sql
