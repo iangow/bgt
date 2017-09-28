43e2fdef8459cb71a87c5a306e2e7d197c9cb4d2
Ian Gow <iandgow@gmail.com>
2017-09-28 11:26:23 +1000

    Fixes to code comparing old and new SAS data.

--- compare_old_new_fog.R
+++ compare_old_new_fog.R
@@ -5,30 +5,53 @@ pg <- dbConnect(PostgreSQL())
 
 calls <-
     tbl(pg, sql("SELECT * FROM streetevents.calls")) %>%
+    select(file_name) %>%
+    distinct() %>%
     collect()
 dbDisconnect(pg)
 
 load("data/fog_data_new.Rdata")
 load("data/fog_data3.rdata")
-fog_data3 %>%
-    as_tibble() %>%
-    anti_join(fog_data, by="file_name") %>%
-    select(file_name) %>%
-    anti_join(calls)
+fog_data3 <- as_tibble(fog_data3)
+fog_data_new <- as_tibble(fog_data_save)
+rm(fog_data_save)
+
+missing_calls <-
+    fog_data3 %>%
+    filter(!is.na(rdq)) %>%
+    anti_join(fog_data_new, by="file_name") %>%
+    inner_join(calls) %>%
+    select(file_name, call_date)
+
+missing_calls %>%
+    mutate(year = format(call_date, "%Y")) %>%
+    count(year) %>%
+    arrange(year)
 
 fog_data3 %>%
-    full_join(fog_data)
+    anti_join(calls) %>%
+    mutate(year = format(call_date, "%Y")) %>%
+    count(year) %>%
+    arrange(year)
 
 old_years <-
     fog_data3 %>%
+    filter(!is.na(gvkey)) %>%
     filter(fog_comp_pres > 0) %>%
     mutate(year = format(call_date, "%Y")) %>%
     count(year) %>%
     arrange(year)
 
-fog_data %>%
+new_years <-
+    fog_data_new %>%
+    filter(!is.na(gvkey)) %>%
     filter(fog_comp_pres > 0) %>%
     mutate(year = format(start_date, "%Y")) %>%
-    count(year) %>%
+    count(year)
+
+combined <-
+    new_years %>%
     full_join(old_years, by="year", suffix=c("_new", "_old")) %>%
     arrange(year)
+
+combined
