e35ab799bc6486fb19c7358a555fc89aa9c5b1f7
Ian Gow <iandgow@gmail.com>
2017-10-30 23:47:50 +1100

    Code to run code for BGT data as a batch.

---
+++ batch_run.R
@@ -0,0 +1,18 @@
+
+
+source('~/git/bgt/fog/get_fog.R', echo=TRUE)
+source('~/git/bgt/fog/get_fog_speaker_data.R', echo=TRUE)
+source('~/git/bgt/fog/get_within_call_data.R', echo=TRUE)
+source('~/git/bgt/fog/get_fog_by_half.R', echo=TRUE)
+# source('~/git/bgt/fog/get_lm_tone_words.R', echo=TRUE)
+source('~/git/bgt/fog/get_tone_data.R', echo=TRUE)
+source('~/git/bgt/fog/get_sent_count.R', echo=TRUE)
+source('~/git/bgt/fog/get_fl_data.R', echo=TRUE)
+source('~/git/bgt/fog/get_long_words.R', echo=TRUE)
+source('~/git/bgt/fog/create_word_freq.R', echo=TRUE)
+source('~/git/bgt/fog/create_sics.R', echo=TRUE)
+source('~/git/bgt/fog/create_jargon_words.R', echo=TRUE)
+source("fog/cast_fog_data.R", echo = TRUE)
+
+# Not yet run
+source("fog/create_fog_data.R", echo = TRUE)
